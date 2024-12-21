c27d5cb610215fb5541dcd71049626054a9c3851
LokeshMehar <meharlokesh395@gmail.com>
2024-12-22 01:02:30 +0530

    added the grid defined bento box kinda dashboard

---
+++ frontend/src/components/pieces/Row3.tsx
@@ -0,0 +1,16 @@
+import DashboardBox from "./DashboardBox";
+
+const Row1 = () => {
+
+
+  return (
+    <>
+    <DashboardBox gridArea="g"></DashboardBox>
+    <DashboardBox gridArea="h"></DashboardBox>
+    <DashboardBox gridArea="i"></DashboardBox>
+    <DashboardBox gridArea="j"></DashboardBox>
+    </>
+  );
+};
+
+export default Row1;
