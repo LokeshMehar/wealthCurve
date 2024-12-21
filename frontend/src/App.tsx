f9b3b28a7cb7d0d376787151e3143ba3d560b274
LokeshMehar <meharlokesh395@gmail.com>
2024-12-21 23:40:04 +0530

    basics done now wanna blast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
+import { createTheme } from '@mui/material/styles'
+import { useMemo, useState } from 'react'
+import { themeSettings } from './theme'
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
 
 
 function App() {
-  
+  const theme = useMemo(() => 
+    createTheme(themeSettings),
+    []
+  )
 
   return (
-    <>
-      <div>
-        aaya??
-      </div>
-      
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      here we go
+    </ThemeProvider>
   )
 }
 
