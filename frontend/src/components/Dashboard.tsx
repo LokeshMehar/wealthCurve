a65e26c1ed010d1113f23e2500e150c50ada2c01
LokeshMehar <meharlokesh395@gmail.com>
2024-12-23 18:04:36 +0530

    vapas karne me bhi yaad rkhna hota h point to be noted

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -2,10 +2,6 @@ import { Box, useMediaQuery } from "@mui/material";
 import Row1 from "./pieces/Row1";
 import Row2 from "./pieces/Row2";
 import Row3 from "./pieces/Row3";
-import { useGetKpisQuery } from "@/state/api";
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
-import { useMemo } from "react";
-
 
 const gridTemplateLargeScreens = `
   "a b c"
@@ -55,31 +51,7 @@ const gridTemplateSmallScreens = `
 const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
 
-  const {data} = useGetKpisQuery();
-
-  let revenueExpenses = useMemo(() => {
-      return (
-        data &&
-        data[0].monthlyData.map(({ month, revenue, expenses }) => {
-          return {
-            name: month.substring(0, 3),
-            revenue: revenue,
-            expenses: expenses,
-          };
-        })
-      );
-    }, [data]);
-
-
   return (
-    <>
-      <LineChart width={1300} height={700} data={revenueExpenses}>
-  <Line type="monotone" dataKey="expenses" stroke="#ff0000" />
-  <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
-  <CartesianGrid stroke="#ccc" />
-  <XAxis dataKey="month" /> 
-      <YAxis />
-    </LineChart>
     <Box
       width="100%"
       height="100%"
@@ -103,7 +75,6 @@ const Dashboard = () => {
       <Row2 />
       <Row3 />
     </Box>
-    </>
   );
 };
 
